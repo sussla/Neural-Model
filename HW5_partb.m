82dd5a8647389e978d01cc7a4e596787ffc0bd21
Lauren <sussla@bu.edu>
2018-11-16 11:24:27 -0500

    HW5

--- HW5_partb.m
+++ HW5_partb.m
@@ -3,11 +3,13 @@
 clc
 clear all 
 
+%First g(w)
+
 %Set parameters 
 A = 1; % parameter set by assignment 
 B = 3; % parameter set by assignment 
-F = 0.2; % set parameter by assignment
-SP = [0.2, 0.6, 0.9, 0.6, 0.2, 0.1, 0.4, 0.8, 0.4, 0.1]; %xi pattern indicated in part a of homework 
+F = 0.25; % set parameter by assignment
+SP = [0.7, 0.6, 0.8, 0.9, 0.5, 0.3, 0.5, 0.7, 0.8, 0.4]; %xi pattern indicated in part a of homework 
 
 %set timing and start pattern
 t_start = 0; % begin with an x value of 0, x(0) = 0 
@@ -21,121 +23,152 @@ dx = 0;
 
 for t = 1:(length(time)-1)
     for i = 1:10
+    L = sum(x(1:10~=i,t)); %creating L from the inhibitory term
     if time(t) <= 1
     I = SP(i);
-    L = sum(x(1:10~=i,t)); %creating L from the inhibitory term
     else 
     I = 0;
-    L = sum(x(1:10~=i,t)); %creating L from the inhibitory term
     end
         dx = ((-1 .* A .* x(i,t))+(B -x(i,t)).*(x(i,t)+ I) - (x(i,t).*L)) * t_inc;
         x(i,t+1) = x(i,t) + dx;
     end
 end 
 
+XN = x(:,:)./sum(x(:,:)); %normalizing the X values
 
 figure(1);
-Add_plot = surf(time, 1:10, x);
-Add_plot.FaceAlpha=0.8;
-Add_plot.LineStyle='none';
+First_plot = surf(time, 1:10, XN);
+First_plot.FaceAlpha=0.8;
+First_plot.LineStyle='none';
 xlabel('Time')
 zlabel('Activity')
 ylabel('Input')
 set(gca, 'fontsize',18)
-title('(i) f(w)=w ', 'FontSize',20)
+title('(i) f(w)=w ', 'FontSize',18)
 
 %bar graph for figures
-figure(2)
+figure(2);
 norm = x(:,end)./(sum(x(:,end)));
 bar(1:10,norm);
+xlabel('Index of Neurons')
+ylabel('Activity')
+title('(i) f(w)=w ', 'FontSize',18)
+
+%% Second f(w)
 
 %loop for (ii) where f(w) = w^2
 for t = 1:(length(time)-1) 
     for i = 1:10
+    Sq_X = (x(i,t))^2;
+    Square = x(1:10~=i,t).^2;
+    Sq_L = sum(Square); %creating L from the inhibitory term 
     if time(t) <= 1
     I = SP(i);
-    Sq_X = (x(i,t))^2;
-    Square = x(1:10~=i,t).^2;
-    Sq_L = sum(Square); %creating L from the inhibitory term 
     else 
     I = 0;
-    Sq_X = (x(i,t))^2;
-    Square = x(1:10~=i,t).^2;
-    Sq_L = sum(Square); %creating L from the inhibitory term 
     end
         dx = ((-1 .* A .* x(i,t))+(B -x(i,t)).*(Sq_X+ I) - (x(i,t).*Sq_L)) * t_inc;
         x(i,t+1)= x(i,t) + dx;
     end
 end
 
+XN = x(:,:)./sum(x(:,:)); %normalizing the X values
+
 figure(3);
-Add_plot = surf(time, 1:10, x);
-Add_plot.FaceAlpha=0.8;
-Add_plot.LineStyle='none';
+Second_plot = surf(time, 1:10, XN);
+Second_plot.FaceAlpha=0.8;
+Second_plot.LineStyle='none';
 xlabel('Time')
 zlabel('Activity')
 ylabel('Input')
 set(gca, 'fontsize',18)
-title('(ii) f(w)= w^2 ', 'FontSize',20)
+title('(ii) f(w)= w^2 ', 'FontSize',18)
+
+%bar graph for figures
+figure(4);
+norm = x(:,end)./(sum(x(:,end)));
+bar(1:10,norm);
+axis([0 10 0 1.5]);
+xlabel('Index of Neurons')
+ylabel('Activity')
+title('(ii) f(w)=w^2 ', 'FontSize',18)
+
+%% Third f(w) 
 
 
 %loop for (iii) where f(w) = w/(F + w)
 for t = 1:(length(time)-1)
     for i = 1:10
+    Third_X = (x(i,t))./(F + x(i,t));
+    third = (x(1:10~=i,t)./(F+x(1:10~=i,t)));
+    Third_L = sum(third); %creating L from the inhibitory term
     if time(t) <= 1
     I = SP(i);
-    Third_X = (x(i,t))./(F + x(i,t));
-    third = (x(1:10~=i,t)./(F+x(1:10~=i,t)));
-    Third_L = sum(third); %creating L from the inhibitory term
     else 
     I = 0;
-    Third_X = (x(i,t))./(F + x(i,t));
-    third = (x(1:10~=i,t)./(F+x(1:10~=i,t)));
-    Third_L = sum(third); %creating L from the inhibitory term
     end
         dx = ((-1 .* A .* x(i,t))+(B -x(i,t)).*(Third_X + I) - (x(i,t).*Third_L)) * t_inc;
         x(i,t+1)= x(i,t) + dx;
     end
 end
 
-figure(4);
-Add_plot = surf(time, 1:10, x);
-Add_plot.FaceAlpha=0.8;
-Add_plot.LineStyle='none';
+XN = x(:,:)./sum(x(:,:)); %normalizing the X values
+
+figure(5);
+Third_plot = surf(time, 1:10, XN);
+Third_plot.FaceAlpha=0.8;
+Third_plot.LineStyle='none';
 xlabel('Time')
 zlabel('Activity')
 ylabel('Input')
 set(gca, 'fontsize',18)
-title('(iii) f(w)= w/(F+w)  ', 'FontSize',20)
+title('(iii) f(w)= w/(F+w)  ', 'FontSize',18)
+
+%bar graph for figures
+figure(6);
+norm = x(:,end)./(sum(x(:,end)));
+bar(1:10,norm);
+axis([0 10 0 1]);
+xlabel('Index of Neurons')
+ylabel('Activity')
+title('(iii) f(w)= w/(F+w)  ', 'FontSize',18)
+%% Fourth f(w) 
 
 
 %loop for (iv) where f(w) = w^2/(F + w^2)
 for t = 1:(length(time)-1)
     for i = 1:10
+    IV_X = (x(i,t)^2)./(F + x(i,t)^2);
+    Square = x(1:10~=i,t).^2;
+    iv = (Square)./(F + Square); %creating L from the inhibitory term
+    IV_L = sum(iv);
     if time(t) <= 1
     I = SP(i);
-    IV_X = (x(i,t)^2)./(F + x(i,t)^2);
-    Square = x(1:10~=i,t).^2;
-    iv = (Square)./(F + Square); %creating L from the inhibitory term
-    IV_L = sum(iv);
     else 
     I = 0;
-    IV_X = (x(i,t)^2)./(F + x(i,t)^2);
-    Square = x(1:10~=i,t).^2;
-    iv = (Square)./(F + Square); %creating L from the inhibitory term
-    IV_L = sum(iv);
     end
         dx = ((-1 .* A .* x(i,t))+(B -x(i,t)).*(IV_X + I) - (x(i,t).*IV_L)) * t_inc;
         x(i,t+1)= x(i,t) + dx;
     end
 end
 
-figure(5);
-Add_plot = surf(time, 1:10, x);
-Add_plot.FaceAlpha=0.8;
-Add_plot.LineStyle='none';
+XN = x(:,:)./sum(x(:,:)); %normalizing the X values
+
+figure(7);
+Fourth_plot = surf(time, 1:10, XN);
+Fourth_plot.FaceAlpha=0.8;
+Fourth_plot.LineStyle='none';
 xlabel('Time')
 zlabel('Activity')
 ylabel('Input')
 set(gca, 'fontsize',18)
-title('(iv) f(w)= w^2/(F+w^2)  ', 'FontSize',20)
+title('(iv) f(w)= w^2/(F+w^2)  ', 'FontSize',18)
+
+%bar graph for figures
+figure(8);
+norm = x(:,end)./(sum(x(:,end)));
+bar(1:10,norm);
+axis([0 10 0 1]);
+xlabel('Index of Neurons')
+ylabel('Activity')
+title('(iv) f(w)= w^2/(F+w^2)  ', 'FontSize',18)
--- HW_prep.m
+++ HW_prep.m
@@ -1,4 +1,4 @@
-%%HW5: Recurrent Competitive Fields: Part 2%%
+%%HW5: Recurrent Competitive Fields%%
 
 clc
 clear all 
@@ -12,27 +12,20 @@ SP = [0.2, 0.6, 0.9, 0.6, 0.2, 0.1, 0.4, 0.8, 0.4, 0.1]; %xi pattern indicated i
 %set timing and start pattern
 t_start = 0; % begin with an x value of 0, x(0) = 0 
 t_inc = .001; % increments that I want to count by
-t_end = 10; % final x value  
+t_end = 1; % final x value  
 time = t_start:t_inc:t_end;
 x = zeros(length(SP),length(time));
 dx = 0;
 
 %loop for (i) where f(w) = w
-
-for t = 1:(length(time)-1)
-    for i = 1:10
-    if time(t) <= 1
+for i = 1:10
     I = SP(i);
-    L = sum(x(1:10~=i,t)); %creating L from the inhibitory term
-    else 
-    I = 0;
-    L = sum(x(1:10~=i,t)); %creating L from the inhibitory term
-    end
+    L = sum(SP(SP~=SP(i))); %creating L from the inhibitory term
+    for t = 1:(length(time)-1)
         dx = ((-1 .* A .* x(i,t))+(B -x(i,t)).*(x(i,t)+ I) - (x(i,t).*L)) * t_inc;
-        x(i,t+1) = x(i,t) + dx;
-    end
-end 
-
+        x(i,t+1)= x(i,t) + dx;
+    end 
+end
 
 figure(1);
 Add_plot = surf(time, 1:10, x);
@@ -44,28 +37,38 @@ ylabel('Input')
 set(gca, 'fontsize',18)
 title('(i) f(w)=w ', 'FontSize',20)
 
-%bar graph for figures
-figure(2)
-norm = x(:,end)./(sum(x(:,end)));
-bar(1:10,norm);
-
 %loop for (ii) where f(w) = w^2
-for t = 1:(length(time)-1) 
-    for i = 1:10
-    if time(t) <= 1
+for i = 1:10
     I = SP(i);
+    L = sum(SP(SP~=SP(i))); %creating L from the inhibitory term
+    Sq_L = L^2;
     Sq_X = (x(i,t))^2;
-    Square = x(1:10~=i,t).^2;
-    Sq_L = sum(Square); %creating L from the inhibitory term 
-    else 
-    I = 0;
-    Sq_X = (x(i,t))^2;
-    Square = x(1:10~=i,t).^2;
-    Sq_L = sum(Square); %creating L from the inhibitory term 
-    end
-        dx = ((-1 .* A .* x(i,t))+(B -x(i,t)).*(Sq_X+ I) - (x(i,t).*Sq_L)) * t_inc;
+    for t = 1:(length(time)-1)
+        dx = ((-1 .* A .* x(i,t))+(B -x(i,t)).*(Sq_X + I) - (x(i,t).*Sq_L)) * t_inc;
         x(i,t+1)= x(i,t) + dx;
-    end
+    end 
+end
+
+figure(2);
+Add_plot = surf(time, 1:10, x);
+Add_plot.FaceAlpha=0.8;
+Add_plot.LineStyle='none';
+xlabel('Time')
+zlabel('Activity')
+ylabel('Input')
+set(gca, 'fontsize',18)
+title('(ii) f(w)= w^2 ', 'FontSize',20)
+
+%loop for (iii) where f(w) = w/(F + w)
+for i = 1:10
+    I = SP(i);
+    L = sum(SP(SP~=SP(i))); %creating L from the inhibitory term
+    Third_L = (L)./(F + L);
+    Third_X = (x(i,t))./(F + x(i,t));
+    for t = 1:(length(time)-1)
+        dx = ((-1 .* A .* x(i,t))+(B -x(i,t)).*(Third_X + I) - (x(i,t).*Third_L)) * t_inc;
+        x(i,t+1)= x(i,t) + dx;
+    end 
 end
 
 figure(3);
@@ -76,26 +79,18 @@ xlabel('Time')
 zlabel('Activity')
 ylabel('Input')
 set(gca, 'fontsize',18)
-title('(ii) f(w)= w^2 ', 'FontSize',20)
+title('(iii) f(w)= w/(F+w)  ', 'FontSize',20)
 
-
-%loop for (iii) where f(w) = w/(F + w)
-for t = 1:(length(time)-1)
-    for i = 1:10
-    if time(t) <= 1
+%loop for (iv) where f(w) = w^2/(F + w^2)
+for i = 1:10
     I = SP(i);
-    Third_X = (x(i,t))./(F + x(i,t));
-    third = (x(1:10~=i,t)./(F+x(1:10~=i,t)));
-    Third_L = sum(third); %creating L from the inhibitory term
-    else 
-    I = 0;
-    Third_X = (x(i,t))./(F + x(i,t));
-    third = (x(1:10~=i,t)./(F+x(1:10~=i,t)));
-    Third_L = sum(third); %creating L from the inhibitory term
-    end
-        dx = ((-1 .* A .* x(i,t))+(B -x(i,t)).*(Third_X + I) - (x(i,t).*Third_L)) * t_inc;
+    L = sum(SP(SP~=SP(i))); %creating L from the inhibitory term
+    IV_L = (L^2)./(F + L^2);
+    IV_X = (x(i,t)^2)./(F + x(i,t)^2);
+    for t = 1:(length(time)-1)
+        dx = ((-1 .* A .* x(i,t))+(B -x(i,t)).*(IV_X + I) - (x(i,t).*IV_L)) * t_inc;
         x(i,t+1)= x(i,t) + dx;
-    end
+    end 
 end
 
 figure(4);
@@ -106,36 +101,5 @@ xlabel('Time')
 zlabel('Activity')
 ylabel('Input')
 set(gca, 'fontsize',18)
-title('(iii) f(w)= w/(F+w)  ', 'FontSize',20)
-
-
-%loop for (iv) where f(w) = w^2/(F + w^2)
-for t = 1:(length(time)-1)
-    for i = 1:10
-    if time(t) <= 1
-    I = SP(i);
-    IV_X = (x(i,t)^2)./(F + x(i,t)^2);
-    Square = x(1:10~=i,t).^2;
-    iv = (Square)./(F + Square); %creating L from the inhibitory term
-    IV_L = sum(iv);
-    else 
-    I = 0;
-    IV_X = (x(i,t)^2)./(F + x(i,t)^2);
-    Square = x(1:10~=i,t).^2;
-    iv = (Square)./(F + Square); %creating L from the inhibitory term
-    IV_L = sum(iv);
-    end
-        dx = ((-1 .* A .* x(i,t))+(B -x(i,t)).*(IV_X + I) - (x(i,t).*IV_L)) * t_inc;
-        x(i,t+1)= x(i,t) + dx;
-    end
-end
-
-figure(5);
-Add_plot = surf(time, 1:10, x);
-Add_plot.FaceAlpha=0.8;
-Add_plot.LineStyle='none';
-xlabel('Time')
-zlabel('Activity')
-ylabel('Input')
-set(gca, 'fontsize',18)
 title('(iv) f(w)= w^2/(F+w^2)  ', 'FontSize',20)
+
